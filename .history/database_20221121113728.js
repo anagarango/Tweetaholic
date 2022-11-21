169fae29ff94171b86c6dc4462c0cbcd6a6d3664
AnaArango <aarango4@my.bcit.ca>
2022-11-21 11:47:31 -0800

    reporting post is working but working on deleting

---
+++ .history/database_20221121113728.js
@@ -0,0 +1,27 @@
+var posts = [
+    {
+        id: 1,
+        name: "James122",
+        title: "Welcome to Tweetaholics",
+        body: "This post is only created just so it can be reported",
+        time: new Date().toLocaleDateString(),
+    },
+    {
+        id: 2,
+        name: "Cassandra887",
+        title: "This is an awesome social media app",
+        body: "This post is only created just so it can be reported",
+        time: new Date().toLocaleDateString()
+    }
+]
+
+var reportPosts = [
+
+]
+
+function DeletePost(id){
+    posts = posts.filter((post) => post)
+}
+exports.DeletePost = DeletePost
+
+module.exports = { posts, reportPosts }
---
+++ .history/server_20221116150706.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
+const { posts, reportPosts } = require('./database')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public')); // static files middleware
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/posts', (req, res) => {
+  res.render('posts.ejs', {
+    posts,
+  })
+})
+
+app.get('/reported', (req, res) => {
+  res.render('reported.ejs', {
+    reportPosts,
+  })
+})
+app.post('/post/report/:id', (req, res) => {
+  const id = +req.params.id
+  const result = posts.filter(posts => posts.id == id);
+  var message = +req.params.name
+  reportPosts.push(result[0])
+  console.log(reportPosts)
+  res.json({message: message});
+  console.log(message)
+  // res.render('posts.ejs', {
+  //   posts,
+  // })
+})
+
+// app.post('/notes/:id/report', (req, res) => {
+//   // const id = +req.params.id
+//   // res.render('posts')
+// })
+
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log(`The app is running on port ${PORT}`)
+})
