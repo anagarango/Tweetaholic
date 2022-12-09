44d6bb40273ceea26ed8f0b51be4df0e59aa7331
William <wchu41@my.bcit.ca>
2022-12-09 03:57:18 -0800

    edit twoot works

--- routes/twoot.js
+++ routes/twoot.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Twoot = require('./../models/posts')
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
   res.send('TEST')
 
-} )
+})
 
 router.get('/new', (req, res) => {
-  
+
   res.render('twoots/new', { newtwoot: new Twoot() })
 
 })
@@ -16,7 +16,7 @@ router.get('/new', (req, res) => {
 router.get('/:id', async (req, res) => {
   const twoot = await Twoot.findById(req.params.id)
   if (twoot == null) res.redirect('/')
-  res.render('twoots/show', {twoot: twoot})
+  res.render('twoots/show', { twoot: twoot })
 
 })
 
@@ -32,10 +32,27 @@ router.post('/', async (req, res) => {
     console.log(newtwoot.id)
     res.redirect(`/twoot/${newtwoot.id}`)
   } catch (e) {
-    res.render('twoots/new', {newtwoot: newtwoot})
+    res.render('twoots/new', { newtwoot: newtwoot })
     console.log(e)
   }
 
+})
+
+router.get('/:id/update', async (req, res) => {
+  let twoot = await Twoot.findById(req.params.id)
+  res.render('twoots/edit', { twoot })
+})
+
+router.post('/update', async (req, res) => {
+
+  const findId = req.body.id
+  const update = {
+    title: req.body.title,
+    body: req.body.body,
+
+  }
+  await Twoot.findByIdAndUpdate(findId, update)
+  res.redirect('/')
 })
 
 module.exports = router
