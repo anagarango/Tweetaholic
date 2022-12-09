09b637f8253c131760ea0443d78156ed2788828c
AnaArango <aarango4@my.bcit.ca>
2022-12-09 01:39:25 -0800

    whoopsie

--- routes/reports.js
+++ routes/reports.js
@@ -3,6 +3,7 @@ const router2 = express.Router()
 const Reported = require('../models/reports')
 const twoot = require('./../models/posts')
 
+
 router2.get('/', async (req, res) => {
   
     const reportPosts = await Reported.find({}).sort({
@@ -23,6 +24,7 @@ router2.get('/', async (req, res) => {
 })
 
 
+
 router2.post('/delete/:id', async (req, res) => {
   
       let thePost = await twoot.findByIdAndDelete({
@@ -31,43 +33,21 @@ router2.post('/delete/:id', async (req, res) => {
     res.redirect('/send-report')
 })
 
-// router2.get('/:id', async (req,res)=>{
-//     res.json({message:"This Post Has Been Reported"})
-// })
+
 
 router2.post('/:id', async (req,res)=>{
     const reportId = req.params.id
     const post = await twoot.findById(reportId)
-    // const reportedDatabase = await Reported.find({title:post.title})
     let reportPostsMap = new Reported({
         idReport: reportId,
-        // title: post.title,
-        // body: post.body,
         createdAt: post.createdAt,
     })
-
-    // let reportPost = await twoot.find({
-    //     '_id': {
-    //         $in: reportId
-    //     }
-    // })
-
-    // console.log(reportPost)
-    
     
     try{
         reportPostsMap = await reportPostsMap.save()
         setTimeout(()=>{
             res.redirect('/')
         },3000)
-        
-        // if(reportedDatabase.length <= 0){
-        //     reportPostsMap = await reportPostsMap.save()
-        //     res.render("reported/reported", {reportPost: post})
-        //     // res.end("thank you for your report")
-        // }else{
-        //     res.end("this post has already been reported")
-        // }
         
     } catch (e) {
         console.log(e)
